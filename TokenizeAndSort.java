8175c319f1998e1f646ae1a2d3b204535068d3af
Puviyarasi-puvi <vpuviyarasi117@gmail.com>
2026-08-14 04:24:19 +0000

    pro7

--- TokenizeAndSort.java
+++ TokenizeAndSort.java
@@ -3,18 +3,50 @@ import java.util.TreeSet;
 public class TokenizeAndSort
 {
 public static void main(String[] args)
-{
 
+ {
 
+	 Scanner scanner = new Scanner(System.in);
 
+	 System.out.println("Enter a line of text:");
 
+	 String inputText = scanner.nextLine();
 
+	 String[] tokens = inputText.split("\\s+");
 
+	 TreeSet<String> tokenSet = new TreeSet<>();
 
+	 for (int i = 0; i <tokens.length;i++)
 
+	 {
 
+		 tokenSet.add(tokens[i]);
 
+	 }
+
+	 System.out.println("Tokens in ascending sorted order:");
+
+	 for (String token : tokenSet)
+
+	 {
+
+		 System.out.println(token);
+
+	 }
+
+	 scanner.close();
+
+ }
 
 }
 
-}
+
+
+
+
+
+
+
+
+
+
